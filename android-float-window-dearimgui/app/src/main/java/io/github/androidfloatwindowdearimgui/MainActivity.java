964d4f5a6fe339643f86037a5b9da166f3d368d0
zhiji <zhiji_0424@qq.com>
2025-07-20 00:08:58 +0800

    加入权限请求

--- android-float-window-dearimgui/app/src/main/java/io/github/androidfloatwindowdearimgui/MainActivity.java
+++ android-float-window-dearimgui/app/src/main/java/io/github/androidfloatwindowdearimgui/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.androidfloatwindowdearimgui;
 
 import android.app.*;
 import android.os.*;
+import android.provider.*;
+import android.content.*;
+import android.net.*;
+import android.widget.*;
 
 
 public class MainActivity extends Activity 
@@ -11,5 +15,37 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        if (have_permission()) {
+            StartFloatWindow();
+        } else {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                       Uri.parse("package:"+getPackageName()));
+            startActivityForResult(intent, 1);
+            // 请求码是1。
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            // 是请求悬浮窗返回的
+            if (have_permission()) {
+                StartFloatWindow();
+            } else {
+                Toast.makeText(this, "没有授权", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    boolean have_permission() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+            || Settings.canDrawOverlays(this);
+    }
+    
+    void StartFloatWindow() {
+        Toast.makeText(this, "成功，仍待完善。", Toast.LENGTH_LONG).show();
     }
 }
