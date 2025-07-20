7852a1abe0e4132858835efa3b69812524f39bae
zhiji <zhiji_0424@qq.com>
2025-07-20 14:09:42 +0800

    纠正拼写疏忽

--- android-float-window-dearimgui/app/src/main/jni/dearimgui/Android.mk
+++ android-float-window-dearimgui/app/src/main/jni/dearimgui/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SRC_FILES := \
     $(IMGUI_DIR)/imgui_demo.cpp \
     $(IMGUI_DIR)/imgui_draw.cpp \
     $(IMGUI_DIR)/imgui_tables.cpp \
-    $(IMGUI_DIR)/imgui_widgets.cppc
+    $(IMGUI_DIR)/imgui_widgets.cpp
 #     $(IMGUI_DIR)/backends/imgui_impl_sdl3.cpp \
 #     $(IMGUI_DIR)/backends/imgui_impl_sdlrenderer3.cpp
 
