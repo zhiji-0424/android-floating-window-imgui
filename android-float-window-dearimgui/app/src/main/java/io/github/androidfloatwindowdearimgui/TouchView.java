d3b8a25e7041f1b635cf0bb7245bf393f02ecf16
zhiji <zhiji_0424@qq.com>
2025-07-22 19:31:53 +0800

    打算使用多个view接受触摸事件

---
+++ android-float-window-dearimgui/app/src/main/java/io/github/androidfloatwindowdearimgui/TouchView.java
@@ -0,0 +1,20 @@
+package io.github.androidfloatwindowdearimgui;
+
+import android.view.*;
+import android.content.*;
+import android.graphics.*;
+
+public class TouchView extends View {
+	public TouchView(Context context) {
+		super(context);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		
+		return super.onTouchEvent(event);
+	}
+	
+	 
+}
